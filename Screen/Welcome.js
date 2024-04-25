c89dc8a4494a8f939165703e8fcc14abeade7d05
juliocesarn0 <julionavarro-09@hotmail.com>
2024-04-25 09:32:31 -0300

    arrumado as fonte no Welcome.js e container usuario v1

--- Screen/Welcome.js
+++ Screen/Welcome.js
@@ -1,25 +1,53 @@
 import React from "react";
-import { View, Text, Button, StatusBar, StyleSheet, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  StatusBar,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+// Adicione a importação para 'useFonts'
+import { useFonts } from "@expo-google-fonts/poppins";
 
 // Importe a imagem da logo
 import Logo from "../assets/logo.png";
+// Importe a imagem do usuário
+import UsuarioImage from "../assets/usuario.png";
 
 export default function WelcomeScreen({ navigation }) {
+  // UseFonts para carregar a fonte
+  let [fontsLoaded] = useFonts({
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null; // Ou uma tela de carregamento
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
         {/* Use a imagem importada como a fonte da imagem */}
-        <Image
-          style={styles.logo}
-          source={Logo}
-        />
+        <Image style={styles.logo} source={Logo} />
       </View>
       <View style={styles.menu}>
         <TouchableOpacity onPress={() => navigation.navigate("LoginUser")}>
           <View style={styles.containerUsuario}>
-            <Text style={styles.containerTexto}>Usuário</Text>
-            <Text>Sou usuario e estou em busca de motoristas.</Text>
+            <View style={styles.usuarioContent}>
+              <Image style={styles.usuarioImage} source={UsuarioImage} />
+              <View style={styles.usuarioText}>
+                {/* Aplicar estilo de textoConteudo */}
+                <Text style={[styles.containerTexto, styles.textoCentralizado]}>
+                  Usuário
+                </Text>
+                <Text style={styles.textoConteudo}>
+                  Sou usuário e estou em busca de motoristas.
+                </Text>
+              </View>
+            </View>
           </View>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => navigation.navigate("LoginUser")}>
@@ -62,20 +90,44 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   containerUsuario: {
-    padding: 48,
-    paddingRight: 110,
-    paddingLeft: 110,
     marginBottom: 30,
     backgroundColor: "#EBEBEB",
+    borderRadius: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    maxWidth: "80%",
+    alignSelf: "center",
   },
   containerMotorista: {
     padding: 48,
     paddingRight: 103,
     paddingLeft: 103,
-    backgroundColor: "#EBEBEB"
+    backgroundColor: "#EBEBEB",
   },
   containerTexto: {
-    fontWeight: 'bold',
-    fontSize: 20,
-  }
+    fontWeight: "regular",
+    fontFamily: "Poppins-Regular",
+  },
+  textoCentralizado: {
+    textAlign: "center",
+    fontSize: 18,
+    color: "#515151",
+  },
+  textoConteudo: {
+    fontSize: 12,
+    color: "#6E6E6E",
+  },
+  usuarioContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+  },
+  usuarioImage: {
+    width: 90,
+    height: 90,
+    marginRight: 10,
+  },
+  usuarioText: {
+    flex: 1,
+  },
 });
